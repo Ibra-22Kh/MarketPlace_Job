c8e173044084e64770d3f14161aaacfa7f1dee39
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-29 23:06:37 +0300

    all the controllers to the go

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js"
+export const register = async (req,res)=>{
+
+    try {
+        const newUser= new User(req.body)
+        await newUser.save();
+        res.status(201).send("User has beend created")
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+}
+
+export const login = async (req,res)=>{
+    
+}
+
+export const logout = async (req,res)=>{
+    
+}
