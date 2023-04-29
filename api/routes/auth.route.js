da6ed9ade33e85c7942153575275651dcf541372
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-29 23:29:27 +0300

    all the changes for the night

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,9 +2,9 @@ import express from "express";
 import {register,login,logout} from "../controllers/auth.controller.js"
 
 const router = express.Router();
-router.get("/register",register)
-router.get("/login",login)
-router.get("/logout",logout)
+router.post("/register", register)
+router.post("/login", login)
+router.post("/logout", logout)
 
 
 
