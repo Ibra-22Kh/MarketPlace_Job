ad46219b7b720fb632adfecbe6e5cdf0c7225829
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-30 23:04:24 +0300

    auth and user have more value now

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,42 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt"
-export const register = async (req,res)=>{
-
+import jwt from "jsonwebtoken";
+export const register = async (req, res, next) => {
     try {
-        const newUser= new User(req.body)
-        await newUser.save();
-        res.status(201).send("User has beend created")
-    } catch (error) {
-        res.status(500).send("Something went wrong")
+      const hash = bcrypt.hashSync(req.body.password, 5);
+      const newUser = new User({
+        ...req.body,
+        password: hash,
+      });
+  
+      await newUser.save();
+      res.status(201).send("User has been created.");
+    } catch (err) {
+      next(err);
     }
-}
+  };
 
 export const login = async (req,res)=>{
-    
+    try {
+        const user = await User.findOne({username:req.body.username});
+        if(!user) return res.status(404).send("User not found!");
+
+        const isCorrect = bcrypt.compare(req.body.password, user.password);
+        if(!isCorrect) return res.status(404).send("Wrong Password or username!");
+
+        const token = jwt.sign({
+            id:user._id,
+            isSeller:user.isSeller
+        },process.env.JWT_KEY)
+        
+        const {password, ...info}= user._doc
+        res.cookie("accessToken",token,{
+            httpOnly:true,
+        }).status(200).send(info)
+
+    } catch (error) {
+        res.status(500).send("something went wrong")
+    }
 }
 
 export const logout = async (req,res)=>{
