77ae1a8fd7690110b0f900a3e51bbf717ff9e174
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-30 23:05:38 +0300

    package added the jsonwebtoken and the cookie parser with modifying the route user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
+import {deleteUser} from "../controllers/user.controller.js"
 
 const router = express.Router();
-router.post("/register",);
+
+router.delete("/:id",deleteUser);
 router.post("/login",);
 
 export default router;
